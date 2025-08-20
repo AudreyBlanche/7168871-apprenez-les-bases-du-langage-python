e018c3d7dd1fe879beba3fd01fb44e623bfd11e2
AudreyBlanche <audrey.donnain@sase-company.com>
2025-08-20 18:31:10 +0000

    Update main.py

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,6 @@
-# Écrivez votre code ici !
+fruits = ["pomme" , "banane" , "orange"]
+fruit.append("kiwi")
+fruits.remove("orange")
+fuits[1] = "ananas"
+print("la longeurs des fruits est", fruits.len() , "element") 
+fruit.sort()
