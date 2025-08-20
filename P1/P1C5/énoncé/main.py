3673541d96858765da6fe6843ae62c61595b3584
AudreyBlanche <audrey.donnain@sase-company.com>
2025-08-20 17:36:08 +0000

    Update main.py
    exo3

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,10 @@
-# Écrivez votre code ici !
+non = "Blanche"
+age = 23
+taille = 1.70
+est_etudiant = True
+print(f"Bonjour, je m'appelle {nom} , j'ai {age} ans et je fais {taille} m.")
+print(f"est etudiant : {est_etudiant}")
+type(nom)
+type(age)
+type(est_etudiant)
+type(taille)
