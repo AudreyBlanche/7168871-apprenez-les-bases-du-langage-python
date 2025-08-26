fa4ec86e124a92fb1d7bf990ad12ce0adceeb54b
AudreyBlanche <audrey.donnain@sase-company.com>
2025-08-26 02:10:32 +0000

    Update main.py

--- P2/P2C3/énoncé/main.py
+++ P2/P2C3/énoncé/main.py
@@ -1,1 +1,17 @@
-# Ecrivez votre code ici
+def salaire_mensuel(salaire_annuel):
+    return salaire_annuel/12
+
+def salaire_hebdomadaire(salaire_mensuel):
+    return salaire_mensuel/4
+
+def salaire_horaire(salaire_hebdomadaire, heures_travaillees):
+    return salaire_hebdomadaire/ heures_travaillees
+
+#Demandez à l'utilisateur de saisir son salaire annuel.
+
+salaire_annuel= float(input("donne moi ton saliare:"))
+heures_travaillees = float(input("donne ton heure hebdomadaire par semaine:"))
+mensuel = salaire_mensuel(salaire_annuel)
+hebdomadaire = salaire_hebdomadaire(mensuel)
+horaire = salaire_horaire(hebdomadaire, heures_travaillees)
+print(f"Mon salaire est, {horaire}")
