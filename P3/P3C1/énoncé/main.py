487cb094898227e1db3116e33c019f04415ddc6a
AudreyBlanche <audrey.donnain@sase-company.com>
2025-09-02 01:05:44 +0000

    Update main.py

--- P3/P3C1/énoncé/main.py
+++ P3/P3C1/énoncé/main.py
@@ -1,1 +1,5 @@
-# Ecrivez votre code ici !
+import operation
+resultat= operation.sum(3,5)
+resultats= operation. multiplication(8,2)
+print(f"le resultat de la somme 3 et 5 est:{resultat}")
+print(f"le resultat de la multiplication 3 et 5 est:{resultats}")
